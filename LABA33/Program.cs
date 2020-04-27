57d94e84f3c00babddce3f8052cda318175d4d2b
dashoook <dashkav2007@gmail.com>
2020-04-27 16:17:22 +0300

    third

---
+++ LABA33/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace LABA33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fromFile = new StreamReader("F:\\C#\\LABA33\\input.txt");
+
+            string sentence = fromFile.ReadLine();
+            fromFile.Close();
+
+            string[] words = sentence.Split(new char[] { ' ', ':', ';', '.', ',', '?', '!', '(', ')', '{', '}', '[', ']', '@', '#', '№', '$', '^', '%', '&', '*' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string maxLength ="";
+
+            foreach (string word in words)
+            {
+                if (word.Length >= maxLength.Length)
+                {
+                    maxLength = word;
+                }
+            }
+            File.WriteAllText("F:\\C#\\LABA33\\output.txt", string.Join(" ", maxLength));
+
+        }
+    }
+}
+
